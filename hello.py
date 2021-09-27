b67b242b094219e5980cac1821e26504126b0aea
hellohelen629 <helenvaj2010@gmail.com>
2021-09-27 15:51:10 -0400

    first file

---
+++ hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 19 15:28:38 2021
+
+@author: zhanghanqing
+"""
+
+print('hello world')
+print('I like 6.00.1x!')
+
+
+
+x=6
+
+if x!= 5:
+    print('No')
+
